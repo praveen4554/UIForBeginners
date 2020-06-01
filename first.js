46e164cab391db02564613a5648b136159d0d9cd
praveen kumar <praveen4554@gmail.com>
2020-06-01 08:42:10 -0500

    jaascript first class

---
+++ first.js
@@ -0,0 +1,36 @@
+
+        //   var a=10;
+        //   var b = "Hello";
+        //   var c = true;
+        //   var d = undefined;
+        //   var e = null;
+        //   var a = "Hello world";
+          var a = 10;
+          var b = 20;
+        //   console.log(a+b);
+        //   console.log(a-b); 10+11 21 10+10+11 10+11+11
+        // a+a+++b+b--+a+b
+            console.log(a+++a+a);
+          console.log(typeof "Hello");
+          document.write(a);
+         console.log('Hello world');
+          function sayHello(){
+              alert('Hello world');
+          }
+
+          /**
+           * Numbers: 1,2,3,
+           * Strings: ''
+           * Boolean: true or false  1 or 0
+           * undefined
+           * Null
+           * Symbol
+           * add +
+           * subtraction -
+           * multplication *
+           * divison /
+           * reminder 5%2 - 1
+           * post increment a++
+           * post decrement a--
+           * predecrment --a
+           */
