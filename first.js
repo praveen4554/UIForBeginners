bb3a018d8c42374c96ee67dd85d4a7d3349fe3d1
praveen kumar <praveen4554@gmail.com>
2020-06-02 08:43:27 -0500

    logical operators

--- first.js
+++ first.js
@@ -9,11 +9,55 @@
           var b = 20;
         //   console.log(a+b);
         //   console.log(a-b); 10+11 21 10+10+11 10+11+11
-        // a+a+++b+b--+a+b
-            console.log(a+++a+a);
-          console.log(typeof "Hello");
-          document.write(a);
-         console.log('Hello world');
+        // a+a+++b+b--+a+b - 10+10+20+20+11+19
+        // 10+10+21+21+10+20
+        // a+a - 20
+        // b= ++b - 21
+        // 20+21 - 41
+        // 41+21 - 62
+        // b = 20
+        // 10
+        // 20
+        /**
+         * var a= 10;
+         * var b= 20;
+         * var result = ++b-(--a)+a+++b+++++a+b-a
+         * 65
+         * 65
+         * 66
+         * 21-9+9+21+11+22-11
+         * 64
+         */
+          // var a= false;
+          // var b = true;
+          // console.log(a && b); // if both a and b is true then it will returns true
+          // console.log(a || b); // if both a and b is false it returns false 
+          // console.log(!a);
+
+           var a = 10;
+           var b = 20;
+          //  console.log(a === b); //false
+          //  console.log(!(a > 10)); // true
+          //  console.log((a>10) || (b <20)); // false
+          //  console.log((a>=10) || (b <20)); //true
+          //  console.log((a>10) && (b <20)); // false
+          //  console.log((a>=10) && (b <20)); // false
+          //  console.log((a>=10) && (b <=20)); // true
+          //  console.log(((a>=10) && (b <=20)) || ((a>=10) && (b <20))); //true
+
+          //  a+=b;
+          //  a= a+b;
+           //ternary operator
+           console.log(a >= 10 ? 'Hello' : 'world');
+          
+          //  console.log( a == b)
+          //  console.log( a!= b);
+          //  console.log( a === b);
+          //  console.log( a!== b);
+        //     console.log(a+ a+(++b)+ b--+a+b);
+        //   console.log(typeof "Hello");
+        //   document.write(a);
+        //  console.log('Hello world');
           function sayHello(){
               alert('Hello world');
           }
