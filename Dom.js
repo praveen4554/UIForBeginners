25ac5f9f6e7b8f3b0d4454c7eea0db7474c33c05
praveen kumar <praveen4554@gmail.com>
2020-06-18 08:55:00 -0500

    document query selector

--- Dom.js
+++ Dom.js
@@ -19,6 +19,7 @@ function changeColor() {
    // btn.addEventListener("click",test);
     // document.getElementById("new").appendChild(btn);
     // removeChild
+    //[10,20,30].sum(); -- 60
     document.getElementsByTagName("body")[0].appendChild(btn);
 
     for(var i=0;i<document.getElementsByTagName("div").length;i++) {
@@ -31,3 +32,52 @@ function changeColor() {
         }
     }
 }
+
+/**
+ * 1. add we need to create button name called test and we are appended to body
+ * 2. i need to check whether we have a button name called test 
+ */
+function add(){
+    var buttons = document.getElementsByTagName("BUTTON");
+    console.log(buttons);
+    for(var i=0;i<buttons.length;i++){
+       if(buttons[i].innerHTML === 'TEST')
+          break;
+    }
+    if( i === buttons.length) {
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "TEST";
+    btn.setAttribute('class','test');
+    btn.addEventListener("click",function(){
+        test();
+    });
+    document.getElementsByTagName("body")[0].appendChild(btn);
+    } else {
+        buttons[buttons.length-1].remove();
+    }
+    // if(document.getElementsByTagName("BUTTON").length === 4){
+    //  var btn = document.createElement("BUTTON");
+    // btn.innerHTML = "TEST";
+    // btn.addEventListener("click",function(){
+    //     test();
+    // });
+    // document.getElementsByTagName("body")[0].appendChild(btn);
+    // } 
+}
+
+function remove(){
+    console.log(document.querySelector("body > .test"))
+    for(var i=0;i< document.querySelectorAll(".child > a").length; i++){
+        document.querySelectorAll(".child > a")[i].style.color = "red";
+    }
+   // document.querySelector(".child > a").style.color = "red";
+   // document.getElementsByTagName("BUTTON")[4].remove();
+}
+
+/**
+ * 1. document.getElementById - single node
+ * 2. document.getElementByClassName - Array of nodes
+ * 3. document.getElementByTagName - Array of nodes
+ * 4. document.querySelector - single of node
+ * 5. document.querySelectorAll - Array of nodes
+ */
